b8b25b32d4ed3c975a7fefe87112a736624a4397
whastica <drlook92@gmail.com>
2025-05-10 22:37:29 -0500

    Se Agrego config CORS

---
+++ src/main/java/com/whalensoft/backendMarket/webConfig.java
@@ -0,0 +1,22 @@
+package com.whalensoft.backendMarket;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class webConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("*");
+            }
+        };
+    }
+}
